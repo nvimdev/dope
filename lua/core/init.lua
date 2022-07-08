f62c399d4704bd444929691669a02e1972e6c2a0
Raphael <glephunter@gmail.com>
2022-07-08 15:22:10 +0800

    remove head option

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -49,8 +49,6 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
-vim.g.did_load_filetypes = 0
-vim.g.do_filetype_lua = 1
 
 local pack = require('core.pack')
 
