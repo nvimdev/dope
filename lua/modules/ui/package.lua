0d9ee90d00570167c0e34aa2c8ccb410f2f16c06
TaoZh <76530956+RakerZh@users.noreply.github.com>
2023-01-08 00:09:49 -0500

    ui: update nvim-tree and nvim-web-devicons repo name (#33)

--- lua/modules/ui/package.lua
+++ lua/modules/ui/package.lua
@@ -6,10 +6,10 @@ package({ 'glepnir/zephyr-nvim', config = conf.zephyr })
 package({ 'glepnir/dashboard-nvim', config = conf.dashboard })
 
 package({
-  'kyazdani42/nvim-tree.lua',
+  'nvim-tree/nvim-tree.lua',
   cmd = 'NvimTreeToggle',
   config = conf.nvim_tree,
-  dependencies = 'kyazdani42/nvim-web-devicons',
+  dependencies = 'nvim-tree/nvim-web-devicons',
 })
 
-package({ 'akinsho/nvim-bufferline.lua', config = conf.nvim_bufferline, dependencies = 'kyazdani42/nvim-web-devicons' })
+package({ 'akinsho/nvim-bufferline.lua', config = conf.nvim_bufferline, dependencies = 'nvim-tree/nvim-web-devicons' })
