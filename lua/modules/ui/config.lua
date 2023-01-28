75e7e93cf4fc1d29a3f20add205beeb084d837e0
xiejie <imxiejie@gmail.com>
2023-01-28 12:40:21 +0800

    update config

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -6,20 +6,35 @@ end
 
 function config.dashboard()
   local db = require('dashboard')
-  db.custom_center = {
-    {
-      icon = '  ',
-      desc = 'Update Plugins                          ',
-      shortcut = 'SPC p u',
-      action = 'Lazy update',
+  db.setup({
+    theme = 'hyper',
+    config = {
+      week_header = {
+        enable = true,
+      },
+      shortcut = {
+        { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+        {
+          desc = ' Files',
+          group = 'Label',
+          action = 'Telescope find_files',
+          key = 'f',
+        },
+        {
+          desc = ' Apps',
+          group = 'DiagnosticHint',
+          action = 'Telescope app',
+          key = 'a',
+        },
+        {
+          desc = ' dotfiles',
+          group = 'Number',
+          action = 'Telescope dotfiles',
+          key = 'd',
+        },
+      },
     },
-    {
-      icon = '  ',
-      desc = 'Find File                              ',
-      action = 'Telescope find_files find_command=rg,--hidden,--files',
-      shortcut = 'SPC f f',
-    },
-  }
+  })
 end
 
 function config.nvim_bufferline()
